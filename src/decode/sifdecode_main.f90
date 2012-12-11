c8da6c73194424aa7c7ce743b40414dd1ae3ed99
nimg <nimg@ca3beb79-f7c6-489d-80df-314e45fd556d>
2012-12-11 07:34:38 +0000

    reorganisation of sifdecode module install/uninstall scripts added minor upgrades to select routines and makemasters
    git-svn-id: http://ccpforge.cse.rl.ac.uk/svn/cutest/sifdecode/trunk@16 ca3beb79-f7c6-489d-80df-314e45fd556d

--- src/decode/sifdecode_main.f90
+++ src/decode/sifdecode_main.f90
@@ -1,200 +1,192 @@
-C     ( Last modified on 7 Dec 2001 at 09:05:00 )
-      PROGRAM       SIFDEC
-C
-C  This is the main program for running the SIF decoder for the GALAHAD and 
-C  CUTEr optimization packages. It calls the driver routine SDLANC which does 
-C  all the work. The purpose of this main program is to open and close all 
-C  files, and to care for the proper filenames when possible.
-C
-C  Nick Gould, for CGT Productions.
-C  December 7th, 1990.
-C
-C
-      INTEGER       IINGPS, IOUTDA, IINFN , IOUTFN, IOUTRA, IINGR
-      INTEGER       IOUTGR, IOUT  , IOUTEX, NEWL  , I
-      INTEGER       IINEX , IPRINT, INFORM, IALGOR, IOUTFF, IOUTFD
-      INTEGER       IAUTO , IOUTGF, IOUTGD, IOUTEM, IOUTEA, IAD0
-      LOGICAL       NONAME, SINGLE
-      EXTERNAL      SDLANC
-C
-C  ASSIGN THE STANDARD OUTPUT UNIT NUMBERS.
-C
-      PARAMETER   ( IOUT = 6 )
-      CHARACTER * 10 PRB
-      CHARACTER * 10 PBNAME
-      CHARACTER * 24 PRBDAT, PRBFN , PRBRA , PRBOUT, PRBGR , PRBET
-      CHARACTER * 24 PRBEX, PRBFF , PRBFD , PRBGF , PRBGD , PRBEA
-C
-C  ASSIGN THE REMAINING I/O UNIT NUMBERS.
-C
-      PARAMETER   ( IOUTDA = 55, IINGPS = 61 )
-      PARAMETER   ( IOUTFN = 52, IOUTRA = 53, IOUTGR = 54 )
-      PARAMETER   ( IOUTEX = 57, IOUTFD = 59, IOUTEA = 66 )
-      PARAMETER   ( IOUTGD = 63, IOUTEM = 67 )
-      PARAMETER   ( IOUTFF = 0 , IOUTGF = 0 )
-C     PARAMETER   ( IOUTFF = 58, IOUTGD = 63 )
-      PARAMETER   ( IINFN  = IINGPS,  IINGR = IINGPS, IINEX = IINGPS )
-      PARAMETER   ( NONAME = .FALSE. )
-C
-C  READ PROBLEM'S NAME, BUILD DEFAULT FILE NAMES AND ASSIGN
-C  THE ACTUAL VALUES USED.
-C
-      READ ( 5, 1000 ) PRB
-C      WRITE( IOUT, 2000 ) PRB
-      DO 10 I = 1, 10
-         IF ( PRB( I: I ) .EQ. ' ' ) THEN
-           NEWL = I - 1
-           GO TO 20
-         END IF
-   10 CONTINUE
-      NEWL = 10
-   20 CONTINUE
-C
-C  specify the method to be used (1=SBMIN,2=AUGLG,3=BARIA).
-C
-      READ( 5, 1020 ) IALGOR
-C
-C  specify whether the problem should be described(<0=DEBUG,0=NO,>0=YES)
-C
-      READ( 5, 1030 ) IPRINT
-C
-C  read the actual problem name and use it for initial output
-C
-      READ( 5, 1000 ) PBNAME
-      WRITE( IOUT, 2000 ) PBNAME
-C
-C  specify whether the derivatives are supplied or are to be computed
-C  using automatic differentiation
-C
-      READ( 5, 1020 ) IAUTO
-C
-C  specify whether AD01 or AD02 should be used to perform the
-C  automatic differentiation
-C
-      READ( 5, 1020 ) IAD0
-C
-C  Specify the precision of the output files (single=0,double=1)
-C
-      READ( 5, 1020 ) I
-      SINGLE = I .EQ. 0
-C
-C  Open output files (if needed)
-C
-      PRBDAT = '                       '
-      PRBOUT = '                       '
-      PRBFN  = '                       '
-      PRBRA  = '                       '
-      PRBGR  = '                       '
-      PRBET  = '                       '
-      PRBEX  = '                       '
-      PRBDAT = PRB( 1 : NEWL ) // '.SIF'
-      PRBOUT = 'OUTSDIF.d'
-      PRBFN  = 'ELFUN.f'
-      PRBFF  = 'ELFUNF.f'
-      PRBFD  = 'ELFUND.f'
-      PRBRA  = 'RANGE.f'
-      PRBGR  = 'GROUP.f'
-      PRBGF  = 'GROUPF.f'
-      PRBGD  = 'GROUPD.f'
-      PRBET  = 'SETTYP.f'
-      PRBEX  = 'EXTER.f'
-      PRBEA  = 'EXTERA.f'
-C
-C  OPEN THE RELEVANT FILES - UNIX SYSTEMS.
-C
-      OPEN ( IINGPS, FILE = PRBDAT, FORM = 'FORMATTED',
-     *       STATUS = 'UNKNOWN' )
-      REWIND IINGPS
-      OPEN ( IOUTDA, FILE = PRBOUT, FORM = 'FORMATTED',
-     *       STATUS = 'UNKNOWN' )
-      REWIND IOUTDA
-      OPEN ( IOUTRA, FILE = PRBRA,  FORM = 'FORMATTED',
-     *       STATUS = 'UNKNOWN' )
-      REWIND IOUTRA
-      OPEN ( IOUTEX, FILE = PRBEX,  FORM = 'FORMATTED',
-     *       STATUS = 'UNKNOWN' )
-      REWIND IOUTEX
-      IF ( IAUTO .EQ. 0 ) THEN
-        OPEN ( IOUTFN, FILE = PRBFN,  FORM = 'FORMATTED',
-     *         STATUS = 'UNKNOWN' )
-        REWIND IOUTFN
-        OPEN ( IOUTGR, FILE = PRBGR,  FORM = 'FORMATTED',
-     *         STATUS = 'UNKNOWN' )
-        REWIND IOUTGR
+! THIS VERSION: SIFDECODE 1.0 - 05/12/2012 AT 14:00 GMT.
+
+!-*-*-*-*-*-*-*-*-  S I F D E C O D E  _ m a i n   P R O G R A M  -*-*-*-*-*-*-
+
+!  Copyright reserved, Gould/Orban/Toint, for GALAHAD productions
+!  Principal author: Nick Gould
+
+!  History -
+!   fortran 77 version originally released as part of CUTE, December 1990
+!   Became separate program as part of SifDec, April 2004
+!   Updated fortran 2003 version released December 2012
+
+!  For full documentation, see 
+!   http://galahad.rl.ac.uk/galahad-www/specs.html
+
+      PROGRAM SIFDECODE_main
+
+!  ------------------------------------------------------------------------
+!  this is the main program for running the SIF decoder for the GALAHAD and 
+!  CUTEst optimization packages. It calls the driver routine SDLANC which does 
+!  all the work. The purpose of this main program is to open and close all 
+!  files, and to care for the proper filenames when possible
+!  ------------------------------------------------------------------------
+
+      USE SIFDECODE
+      IMPLICIT NONE
+
+!  local variables
+
+      INTEGER :: i, ad0, auto, print_level, status, algorithm
+      LOGICAL :: single
+
+!  assign the standard output unit numbers
+
+      INTEGER, PARAMETER :: in = 5
+      INTEGER, PARAMETER :: out = 6
+      CHARACTER ( LEN = 10 ) :: pbname = REPEAT( ' ', 10 )
+      CHARACTER ( LEN = 10 ) :: prb = REPEAT( ' ', 10 )
+      CHARACTER ( LEN = 80 ) :: prbdat  = REPEAT( ' ', 80 )
+
+!  assign the remaining i/o unit numbers
+
+      INTEGER, PARAMETER :: ingps = 61
+      INTEGER, PARAMETER :: infn = ingps
+      INTEGER, PARAMETER :: ingr = ingps
+      INTEGER, PARAMETER :: inex = ingps
+
+      INTEGER, PARAMETER :: outda = 55
+      INTEGER, PARAMETER :: outfn = 52
+      INTEGER, PARAMETER :: outra = 53
+      INTEGER, PARAMETER :: outgr = 54
+      INTEGER, PARAMETER :: outex = 57
+      INTEGER, PARAMETER :: outfd = 59
+      INTEGER, PARAMETER :: outea = 66
+      INTEGER, PARAMETER :: outgd = 63
+      INTEGER, PARAMETER :: outem = 67
+      INTEGER, PARAMETER :: outff = 0
+      INTEGER, PARAMETER :: outgf = 0
+
+!  assign file names
+
+      CHARACTER ( LEN = 24 ), PARAMETER :: prbout = 'OUTSDIF.d              '
+      CHARACTER ( LEN = 24 ), PARAMETER :: prbfn  = 'ELFUN.f                '
+      CHARACTER ( LEN = 24 ), PARAMETER :: prbff  = 'ELFUNF.f               '
+      CHARACTER ( LEN = 24 ), PARAMETER :: prbfd  = 'ELFUND.f               '
+      CHARACTER ( LEN = 24 ), PARAMETER :: prbra  = 'RANGE.f                '
+      CHARACTER ( LEN = 24 ), PARAMETER :: prbgr  = 'GROUP.f                '
+      CHARACTER ( LEN = 24 ), PARAMETER :: prbgf  = 'GROUPF.f               '
+      CHARACTER ( LEN = 24 ), PARAMETER :: prbgd  = 'GROUPD.f               '
+      CHARACTER ( LEN = 24 ), PARAMETER :: prbet  = 'SETTYP.f               '
+      CHARACTER ( LEN = 24 ), PARAMETER :: prbex  = 'EXTER.f                '
+      CHARACTER ( LEN = 24 ), PARAMETER :: prbea  = 'EXTERA.f               '
+
+      LOGICAL, PARAMETER :: noname = .FALSE.
+
+!  read the problem's name, build default file names and assign 
+!  the actual values used
+
+      READ ( in, "( A10 )" ) prb
+      prbdat = TRIM( prb ) // '.SIF'
+
+!  specify the method to be used (1=SBMIN, 2=AUGLG, 3=BARIA).
+
+      READ( in, "( I2 )" ) algorithm
+
+!  specify whether the problem should be described(<0=DEBUG, 0=NO, >0=YES)
+
+      READ( in, "( I6 )" ) print_level
+
+!  read the actual problem name and use it for initial output
+
+      READ( in, "( A10 )" ) pbname
+      WRITE( out, "( /, ' Problem name: ', A )" ) TRIM( pbname )
+
+!  specify whether the derivatives are supplied or are to be computed
+!  using automatic differentiation
+
+      READ( in, "( I2 )" ) auto
+
+!  specify whether AD01 or AD02 should be used to perform the
+!  automatic differentiation
+
+      READ( in, "( I2 )" ) ad0
+
+!  specify the precision of the output files (single=0,double=1)
+
+      READ( in, "( I2 )" ) i
+      single = i == 0
+
+!  open the relevant files - unix systems
+
+      OPEN( ingps, FILE = prbdat, FORM = 'FORMATTED', STATUS = 'UNKNOWN' )
+      REWIND ingps
+      OPEN( outda, FILE = prbout, FORM = 'FORMATTED', STATUS = 'UNKNOWN' )
+      REWIND outda
+      OPEN( outra, FILE = prbra,  FORM = 'FORMATTED', STATUS = 'UNKNOWN' )
+      REWIND outra
+      OPEN( outex, FILE = prbex,  FORM = 'FORMATTED', STATUS = 'UNKNOWN' )
+      REWIND OUTEX
+      IF ( auto == 0 ) THEN
+        OPEN( outfn, FILE = prbfn,  FORM = 'FORMATTED', STATUS = 'UNKNOWN' )
+        REWIND OUTFN
+        OPEN( outgr, FILE = prbgr,  FORM = 'FORMATTED', STATUS = 'UNKNOWN' )
+        REWIND outgr
       ELSE
-        IF ( IOUTFF .GT. 0 ) THEN
-          OPEN ( IOUTFF, FILE = PRBFF,  FORM = 'FORMATTED',
-     *           STATUS = 'UNKNOWN' )
-          REWIND IOUTFF
+        IF ( outff > 0 ) THEN
+          OPEN( outff, FILE = prbff,  FORM = 'FORMATTED', STATUS = 'UNKNOWN' )
+          REWIND outff
         END IF
-        OPEN ( IOUTFD, FILE = PRBFD,  FORM = 'FORMATTED',
-     *         STATUS = 'UNKNOWN' )
-        REWIND IOUTFD
-        IF ( IOUTGF .GT. 0 ) THEN
-          OPEN ( IOUTGF, FILE = PRBGF,  FORM = 'FORMATTED',
-     *           STATUS = 'UNKNOWN' )
-          REWIND IOUTGF
+        OPEN( outfd, FILE = prbfd,  FORM = 'FORMATTED', STATUS = 'UNKNOWN' )
+        REWIND outfd
+        IF ( outgf > 0 ) THEN
+          OPEN( outgf, FILE = prbgf,  FORM = 'FORMATTED', STATUS = 'UNKNOWN' )
+          REWIND outgf
         END IF
-        OPEN ( IOUTGD, FILE = PRBGD,  FORM = 'FORMATTED',
-     *         STATUS = 'UNKNOWN' )
-        REWIND IOUTGD 
-        OPEN ( IOUTEA, FILE = PRBEA,  FORM = 'FORMATTED',
-     *         STATUS = 'UNKNOWN' )
-        REWIND IOUTEA
+        OPEN( outgd, FILE = prbgd,  FORM = 'FORMATTED', STATUS = 'UNKNOWN' )
+        REWIND outgd 
+        OPEN( outea, FILE = prbea,  FORM = 'FORMATTED', STATUS = 'UNKNOWN' )
+        REWIND outea
       END IF
-      OPEN( UNIT = IOUTEM )
-C
-C     DO THE WORK
-C
-      CALL SDLANC( IINGPS, IOUTDA, IINFN , IOUTFN, IOUTFF, IOUTFD, 
-     *             IOUTRA, IINGR , IOUTGR, IOUTGF, IOUTGD, 
-     *             IINEX , IOUTEX, IOUTEM, IOUTEA, IPRINT, IOUT  , 
-     *             NONAME, IALGOR, IAUTO , IAD0  , SINGLE, INFORM )
-C
-C   CLOSE THE OPENED FILES
-C
-      CLOSE( IINGPS )
-      IF ( INFORM .EQ. 0 ) THEN
-        CLOSE( IOUTDA )
-        CLOSE( IOUTRA )
-        CLOSE( IOUTEX )
-        IF ( IAUTO .EQ. 0 ) THEN
-          CLOSE( IOUTFN )
-          CLOSE( IOUTGR )
+      OPEN( UNIT = outem )
+
+!  decode the problem
+
+      CALL SIFDECODE_sdlanc( ingps, outda, infn, outfn, outff, outfd, outra,   &
+                             ingr, outgr, outgf, outgd, inex, outex, outem,    &
+                             outea, print_level, out, noname, algorithm,       &
+                             auto, ad0, single, status )
+
+!  close the opened files
+
+      CLOSE( ingps )
+      IF ( status == 0 ) THEN
+        WRITE( out, "( ' File successfully decoded' )" )
+        CLOSE( outda )
+        CLOSE( outra )
+        CLOSE( outex )
+        IF ( auto == 0 ) THEN
+          CLOSE( outfn )
+          CLOSE( outgr )
         ELSE
-          IF ( IOUTFF .GT. 0 ) CLOSE( IOUTFF )
-          CLOSE( IOUTFD )
-          IF ( IOUTGF .GT. 0 ) CLOSE( IOUTGF )
-          CLOSE( IOUTGD )
-          CLOSE( IOUTEA )
+          IF ( outff > 0 ) CLOSE( outff )
+          CLOSE( outfd )
+          IF ( outgf > 0 ) CLOSE( outgf )
+          CLOSE( outgd )
+          CLOSE( outea )
         END IF
-C
-C  IF AN ERROR HAS BEEN DISCOVERED, DELETE THE OUTPUT FILES.
-C
+
+!  if an error has been discovered, delete the output files
+
       ELSE
-        CLOSE( IOUTDA, STATUS = 'DELETE' )
-        CLOSE( IOUTRA, STATUS = 'DELETE' )
-        IF ( IAUTO .EQ. 0 ) THEN
-          CLOSE( IOUTFN, STATUS = 'DELETE' )
-          CLOSE( IOUTGR, STATUS = 'DELETE' )
-          CLOSE( IOUTEX, STATUS = 'DELETE' )
+        WRITE( out, "( ' Decoding failure, status = ', I0 )" ) status
+        CLOSE( outda, STATUS = 'DELETE' )
+        CLOSE( outra, STATUS = 'DELETE' )
+        IF ( auto == 0 ) THEN
+          CLOSE( outfn, STATUS = 'DELETE' )
+          CLOSE( outgr, STATUS = 'DELETE' )
+          CLOSE( outex, STATUS = 'DELETE' )
         ELSE
-          IF ( IOUTFF .GT. 0 ) CLOSE( IOUTFF, STATUS = 'DELETE' )
-          CLOSE( IOUTFD, STATUS = 'DELETE' )
-          IF ( IOUTGF .GT. 0 ) CLOSE( IOUTGF, STATUS = 'DELETE' )
-          CLOSE( IOUTGD, STATUS = 'DELETE' )
-          CLOSE( IOUTEA, STATUS = 'DELETE' )
+          IF ( outff > 0 ) CLOSE( outff, STATUS = 'DELETE' )
+          CLOSE( outfd, STATUS = 'DELETE' )
+          IF ( outgf > 0 ) CLOSE( outgf, STATUS = 'DELETE' )
+          CLOSE( outgd, STATUS = 'DELETE' )
+          CLOSE( outea, STATUS = 'DELETE' )
         END IF
       END IF
-      CLOSE( IOUTEM, STATUS = 'DELETE' )
+      CLOSE( outem, STATUS = 'DELETE' )
       STOP
- 1000 FORMAT( A10 )
- 1020 FORMAT( I2 )
- 1030 FORMAT( I6 )
- 2000 FORMAT( /, ' Problem name: ', A10 )
 
-C
-C END OF PROGRAM SIFDEC
-C
+!  end of program SIFDEC
 
-      END
+      END PROGRAM SIFDECODE_main
